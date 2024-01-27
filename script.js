f9f86cb47222c5dd19031d1d3e7373fa0f1f2220
lppellin <thelucaspellin@gmail.com>
2024-01-27 10:35:32 -0300

    12

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var suspects = ['PREFEITO', 'MORDOMO', 'COZINHEIRO', 'FLORISTA', 'MÉDICA', 'DANÇARINA', 'COVEIRO', 'CIGANA'];
-var weapons = ['ESPINGARDA', 'PÁ', 'PÉ DE CABRA', 'TESOURA', 'ARMA QUÍMICA', 'VENENO', 'SOCO INGLÊS', 'FACA'];
-var locations = ['PREFEITURA', 'RESTAURANTE', 'FLORICULTURA', 'BOATE', 'HOSPITAL', 'MANSÃO', 'CEMITÉRIO', 'PRAÇA', 'HOTEL', 'BANCO', 'ESTAÇÃO'];
+const suspects = ['PREFEITO', 'MORDOMO', 'COZINHEIRO', 'FLORISTA', 'MÉDICA', 'DANÇARINA', 'COVEIRO', 'CIGANA'];
+const weapons = ['ESPINGARDA', 'PÁ', 'PÉ DE CABRA', 'TESOURA', 'ARMA QUÍMICA', 'VENENO', 'SOCO INGLÊS', 'FACA'];
+const locations = ['PREFEITURA', 'RESTAURANTE', 'FLORICULTURA', 'BOATE', 'HOSPITAL', 'MANSÃO', 'CEMITÉRIO', 'PRAÇA', 'HOTEL', 'BANCO', 'ESTAÇÃO'];
 
 function createItems(listId, items) {
- var ul = document.getElementById(listId);
+ const ul = document.getElementById(listId);
  items.forEach(function(item) {
-   var li = document.createElement("li");
+   const li = document.createElement("li");
    li.className = "list-group-item";
    li.innerHTML = item + ' <input type="checkbox" class="checkbox">' + ' <input type="text" class="input-size" placeholder="">';
    ul.appendChild(li);
@@ -17,38 +17,38 @@ createItems('weaponsList', weapons);
 createItems('locationsList', locations);
 
 window.onload = function() {
- // Recuperar o estado das caixas de seleção do localStorage
- var checkboxes = document.querySelectorAll('.list-group-item input[type=checkbox]');
+// Recuperar o estado das caixas de seleção do localStorage
+ const checkboxes = document.querySelectorAll('.list-group-item input[type=checkbox]');
  checkboxes.forEach(function(checkbox, index) {
    checkbox.id = 'checkbox' + index;
-   var savedState = localStorage.getItem(checkbox.id);
+   const savedState = localStorage.getItem(checkbox.id);
    if (savedState !== null) {
      checkbox.checked = savedState === 'true';
    }
 
-   // Atualizar o localStorage sempre que o estado da caixa de seleção mudar
+// Atualizar o localStorage sempre que o estado da caixa de seleção mudar
    checkbox.addEventListener('change', function() {
      localStorage.setItem(checkbox.id, checkbox.checked);
    });
  });
 
- // Campo de entrada de texto
- var inputs = document.querySelectorAll('.list-group-item input[type=text]');
+// Campo de entrada de texto
+ const inputs = document.querySelectorAll('.list-group-item input[type=text]');
  inputs.forEach(function(input, index) {
    input.id = 'input' + index;
-   var savedValue = localStorage.getItem(input.id);
+   const savedValue = localStorage.getItem(input.id);
    if (savedValue !== null) {
      input.value = savedValue;
    }
 
-   // Atualizar o localStorage sempre que o valor do campo de entrada de texto mudar
+// Atualizar o localStorage sempre que o valor do campo de entrada de texto mudar
    input.addEventListener('input', function() {
      localStorage.setItem(input.id, input.value);
    });
  });
 
- // Botão para restaurar o estado das caixas de seleção e o valor do campo de entrada de texto
- var restoreButton = document.createElement('button');
+// Botão para restaurar o estado das caixas de seleção e o valor do campo de entrada de texto
+ const restoreButton = document.createElement('button');
  restoreButton.textContent = 'LIMPAR';
  restoreButton.className = 'restore-button';
  restoreButton.onclick = function() {
